f1fbc3a716513b6ba8be6321893b43a02811d0bf
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2022-06-07 15:38:10 +0530

    navbar and footer update

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,15 +1,29 @@
-var themeBtn = document.querySelector(".themeBtn");
+var themeBtn1 = document.querySelector(".themeBtn1");
+var themeBtn2 = document.querySelector(".themeBtn2");
 var themeLink = document.querySelector("#themeLink");
 
-function changeTheme(){
-    if(themeBtn.classList.contains('fa-moon')){
-        themeBtn.classList.remove('fa-moon');
-        themeBtn.classList.add('fa-lightbulb');
+function changeTheme1(){
+    if(themeBtn1.classList.contains('fa-moon')){
+        themeBtn1.classList.remove('fa-moon');
+        themeBtn1.classList.add('fa-lightbulb');
         themeLink.href = "assets/styles/lightTheme.css";
     }
-    else if(themeBtn.classList.contains('fa-lightbulb')){
-        themeBtn.classList.remove('fa-lightbulb');
-        themeBtn.classList.add('fa-moon');
+    else if(themeBtn1.classList.contains('fa-lightbulb')){
+        themeBtn1.classList.remove('fa-lightbulb');
+        themeBtn1.classList.add('fa-moon');
+        themeLink.href = "assets/styles/darkTheme.css";
+    }
+}
+
+function changeTheme2(){
+    if(themeBtn2.classList.contains('fa-moon')){
+        themeBtn2.classList.remove('fa-moon');
+        themeBtn2.classList.add('fa-lightbulb');
+        themeLink.href = "assets/styles/lightTheme.css";
+    }
+    else if(themeBtn2.classList.contains('fa-lightbulb')){
+        themeBtn2.classList.remove('fa-lightbulb');
+        themeBtn2.classList.add('fa-moon');
         themeLink.href = "assets/styles/darkTheme.css";
     }
 }
